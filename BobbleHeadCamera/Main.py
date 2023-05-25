39c50c56dcd074b869048b508b192821f1824fe9
GareebScientist <110341511+GareebScientist@users.noreply.github.com>
2023-05-25 08:24:32 +0530

    mic inputs working

--- BobbleHeadCamera/Main.py
+++ BobbleHeadCamera/Main.py
@@ -4,6 +4,10 @@ from tkinter import filedialog
 from PIL import Image, ImageTk
 import numpy as np
 import json
+import sounddevice as sd
+import audioop
+import math
+import pyaudio
 
 class ImageCombinationApp:
     def __init__(self):
@@ -12,6 +16,7 @@ class ImageCombinationApp:
         self.images = [None, None, None]
         self.thumbnails = [None, None, None]
         self.image_paths = [None, None, None]
+        self.microphone_inputs = ["Select Microphone"]
 
         # Load image paths from file
         self.load_image_paths()
@@ -34,9 +39,30 @@ class ImageCombinationApp:
         # Create a button to combine and display the images
         self.button_combine_images = tk.Button(self.window, text="Combine Images", command=self.display_images)
         self.button_combine_images.pack(pady=10)
+
+        # Get available microphone inputs
+        self.get_microphone_inputs()
+
+        # Update the tkinter variable after retrieving the microphone inputs
+        self.microphone_variable = tk.StringVar(self.window, self.microphone_inputs[0])
+
+        # Create microphone selection dropdown
+        self.microphone_dropdown = tk.OptionMenu(self.window, self.microphone_variable, *self.microphone_inputs, command=self.start_audio_processing)
+        self.microphone_dropdown.pack(pady=10)
+
         self.display_thumbnails()
 
         self.window.protocol("WM_DELETE_WINDOW", self.close_app)
+
+
+    def get_microphone_inputs(self):
+        audio = pyaudio.PyAudio()
+        num_devices = audio.get_device_count()
+
+        for i in range(num_devices):
+            device_info = audio.get_device_info_by_index(i)
+            if device_info.get('maxInputChannels') > 0:
+                self.microphone_inputs.append(device_info['name'])
 
     def select_image(self, idx):
         path = filedialog.askopenfilename()
@@ -120,8 +146,44 @@ class ImageCombinationApp:
         self.save_image_paths()
         self.window.destroy()
 
+    def start_audio_processing(self, selected_microphone):
+        if selected_microphone != "Select Microphone":
+            selected_microphone_index = -1
+            devices = sd.query_devices()
+            for i, device in enumerate(devices):
+                if device['name'] == selected_microphone:
+                    selected_microphone_index = i
+                    break
+            if selected_microphone_index != -1:
+                sample_rate = int(devices[selected_microphone_index]['default_samplerate'])
+                block_size = 1024
+
+                def callback(indata, frames, time, status):
+                    # Process the recorded audio data here
+                    rms = audioop.rms(indata[:, 0], 2)
+                    # Adjust Image 2 position based on the audio input (e.g., rms value)
+                    # Implement your logic here
+                    # Example: Adjust image position based on rms value
+                    image2_position = math.sin(rms / 1000) * 10  # Modify the formula according to your desired behavior
+
+                    # Update the image position (example: adjust y-coordinate)
+                    image2_resized = self.images[1]
+                    image2_resized = np.roll(image2_resized, int(image2_position), axis=0)
+                    self.images[1] = image2_resized
+
+                    # Update the displayed thumbnail
+                    self.thumbnails[1] = self.create_thumbnail(image2_resized)
+                    self.labels_thumbnails[1].config(image=self.thumbnails[1])
+                    self.labels_thumbnails[1].image = self.thumbnails[1]
+
+                with sd.InputStream(device=selected_microphone_index, channels=1, samplerate=sample_rate, callback=callback, blocksize=block_size):
+                    self.window.wait_window()
+
 # Create an instance of the ImageCombinationApp class
 app = ImageCombinationApp()
+
+# Get available microphone inputs
+app.get_microphone_inputs()
 
 # Start the UI event loop
 app.window.mainloop()
